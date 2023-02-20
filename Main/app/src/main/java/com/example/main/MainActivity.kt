d3705788813a76682255a772dd0b8059cf878ba4
nulzi <kimty131@naver.com>
2023-02-20 19:17:32 +0900

    # main setting

--- Main/app/src/main/java/com/example/main/MainActivity.kt
+++ Main/app/src/main/java/com/example/main/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.main.databinding.ActivityMainBinding
 
-typealias LumaListener = (luma: Double) -> Unit
 
 class MainActivity : AppCompatActivity() {
 
