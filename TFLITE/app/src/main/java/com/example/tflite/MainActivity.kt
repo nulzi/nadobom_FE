0359ab34e67e7dab20f63049385f9b2064380690
nulzi <kimty131@naver.com>
2023-03-07 16:53:05 +0900

    # tflite
    set camera

--- TFLITE/app/src/main/java/com/example/tflite/MainActivity.kt
+++ TFLITE/app/src/main/java/com/example/tflite/MainActivity.kt
@@ -1,11 +1,109 @@
 package com.example.tflite
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.*
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.view.Surface
+import android.view.TextureView
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var cameraDevice: CameraDevice
+    lateinit var handler: Handler
+    lateinit var cameraManager: CameraManager
+    lateinit var textureView: TextureView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getPermission()
+
+        val handlerThread = HandlerThread("videoThread")
+        handlerThread.start()
+        handler = Handler(handlerThread.looper)
+
+        textureView = findViewById(R.id.textureView)
+        textureView.surfaceTextureListener = object :TextureView.SurfaceTextureListener{
+            override fun onSurfaceTextureAvailable(p0: SurfaceTexture, p1: Int, p2: Int) {
+                openCamera()
+            }
+
+            override fun onSurfaceTextureSizeChanged(p0: SurfaceTexture, p1: Int, p2: Int) {
+            }
+
+            override fun onSurfaceTextureDestroyed(p0: SurfaceTexture): Boolean {
+                return false
+            }
+
+            override fun onSurfaceTextureUpdated(p0: SurfaceTexture) {
+
+            }
+
+        }
+
+        cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    @SuppressLint("MissingPermission")//already check getPermission
+    fun openCamera(){
+        cameraManager.openCamera(cameraManager.cameraIdList[0],object:CameraDevice.StateCallback(){
+            override fun onOpened(p0: CameraDevice) {
+                cameraDevice = p0
+
+                var surfaceTexture = textureView.surfaceTexture
+                var surface = Surface(surfaceTexture)
+
+                var captureRequest = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+                captureRequest.addTarget(surface)
+
+                cameraDevice.createCaptureSession(listOf(surface), object : CameraCaptureSession.StateCallback(){
+                    override fun onConfigured(p0: CameraCaptureSession) {
+                        p0.setRepeatingRequest(captureRequest.build(),null,null)
+                    }
+
+                    override fun onConfigureFailed(p0: CameraCaptureSession) {
+
+                    }
+                },handler)
+            }
+
+            override fun onDisconnected(p0: CameraDevice) {
+            }
+
+            override fun onError(p0: CameraDevice, p1: Int) {
+            }
+
+        },handler)
+    }
+
+    private fun getPermission(){
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 101)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(grantResults[0] != PackageManager.PERMISSION_GRANTED){
+            getPermission()
+        }
     }
 }
