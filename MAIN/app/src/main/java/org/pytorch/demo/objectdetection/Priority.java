5ae80d159431d33e635b575a203c90a0ce1e7607
nulzi <kimty131@naver.com>
2023-05-09 13:48:12 +0900

    Mod: 우선순위 구하는 함수에 장애물들 간의 가중치 추가

--- MAIN/app/src/main/java/org/pytorch/demo/objectdetection/Priority.java
+++ MAIN/app/src/main/java/org/pytorch/demo/objectdetection/Priority.java
@@ -53,29 +53,52 @@ public class Priority {
         return direction;
     }
 
+    private static int getObstacleWeight(String obstacle){
+        if(obstacle.equals("트럭")) return 0;
+        if(obstacle.equals("킥보드") || obstacle.equals("자전거")) return 1;
+        if(obstacle.equals("볼라드") || obstacle.equals("바리케이드")) return 2;
+        return 3;
+    }
     // 이 부분에서 각각의 요소 값들의 이름을 지정
-    static ArrayList<String> priority(ArrayList<Labels> list, int deviceWidth) {
+    static ArrayList<String> priority(ArrayList<Labels> list, int viewWidth, int viewHeight) {
+//        Log.d("MyTag_priority","size: "+viewWidth+", "+viewHeight);
+        if(list.size() == 1){
+            ArrayList<String> results = new ArrayList<>();
+            results.add(directions(list.get(0).x, viewWidth));
+            results.add(list.get(0).name);
+            return results;
+        }
         int firstIndex = 0;
-        int secondIndex = 0;
+        int secondIndex = 1;
         int count = list.size();
 
+//        Log.d("MyTag_priority()","name: "+list.get(0).name+" distance : "+len(list.get(0).x, list.get(0).y, viewWidth, viewHeight)+", x, y : " + list.get(0).x + ", " + list.get(0).y);
         for (int i = 1; i < count; i++) {
             // 최단거리 구해서 작성 (x,y)좌표 이용, 기존 위치 = ?
-            double distance = len(list.get(i).x, list.get(i).y);
-            if (distance < len(list.get(firstIndex).x, list.get(firstIndex).y)) {
+            double distance = len(list.get(i).x, list.get(i).y, viewWidth, viewHeight);
+//            Log.d("MyTag_priority()","name: "+list.get(i).name+"distance : "+distance+", x, y : " + list.get(i).x + ", " + list.get(i).y);
+            if (distance < len(list.get(firstIndex).x, list.get(firstIndex).y, viewWidth, viewHeight)) {
                 secondIndex = firstIndex;
                 firstIndex = i;
             }
-            else if (distance < len(list.get(secondIndex).x, list.get(secondIndex).y)) {
+            else if (distance < len(list.get(secondIndex).x, list.get(secondIndex).y, viewWidth, viewHeight)) {
                 secondIndex = i;
             }
+        }
+        int minLength = 200; // issue 수정 필요
+        int firstWeight = getObstacleWeight(list.get(firstIndex).name);
+        int secondWeight = getObstacleWeight(list.get(secondIndex).name);
+        if(len(list.get(secondIndex).x, list.get(secondIndex).y, viewWidth, viewHeight) - len(list.get(firstIndex).x, list.get(firstIndex).y, viewWidth, viewHeight) < minLength && firstWeight > secondWeight){
+            int temp = firstIndex;
+            firstIndex = secondIndex;
+            secondIndex = temp;
         }
 
         // 마지막에 첫 번째 물체의 방향 및 이름, 두 번째 물체의 방향 및 이름, 마지막으로 2개를 제외한 나머지 개수
         ArrayList<String> results = new ArrayList<>();
-        results.add(directions(list.get(firstIndex).x, deviceWidth));
+        results.add(directions(list.get(firstIndex).x, viewWidth));
         results.add(list.get(firstIndex).name);
-        results.add(directions(list.get(secondIndex).x, deviceWidth));
+        results.add(directions(list.get(secondIndex).x, viewWidth));
         results.add(list.get(secondIndex).name);
         results.add(Integer.toString(count - 2));
 
